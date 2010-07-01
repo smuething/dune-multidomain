401e5876795029b3224d2556797444d0f80d8cfd
Steffen Muething <steffen.muething@ipvs.uni-stuttgart.de>
2010-07-01 07:56:59 +0200

    add new test example reactiondiffusion for coupled reaction-diffusion / diffusion problem
    The new problem is a simple extension of the poisson
    example. Instead of solving an identical poisson
    problem on both parts of the domain, it creates a
    second GridFunctionSpace on the left part and solves
    the reaction-diffusion example problem from pdelab-howto
    on that subdomain. On the other subdomain, the poisson
    operator is replaced by the diffusion operator to
    support a diffusion coefficient different from 1.
    Coupling is again implicit, i.e. built into the
    underlying space.

--- test/reactiondiffusion.cc
+++ test/reactiondiffusion.cc
@@ -6,6 +6,7 @@
 #include <dune/pdelab/finiteelementmap/q1fem.hh>
 #include <dune/pdelab/backend/istlvectorbackend.hh>
 #include <dune/pdelab/backend/istlmatrixbackend.hh>
+#include <dune/pdelab/finiteelementmap/q22dfem.hh>
 #include <dune/pdelab/multidomain/subproblemgridfunctionspace.hh>
 #include <dune/pdelab/multidomain/subproblemlocalfunctionspace.hh>
 #include <dune/pdelab/multidomain/multidomaingridoperatorspace.hh>
@@ -15,21 +16,57 @@
 #include <dune/pdelab/multidomain/interpolate.hh>
 #include <dune/pdelab/backend/istlsolverbackend.hh>
 #include <dune/pdelab/localoperator/laplacedirichletp12d.hh>
-#include <dune/pdelab/localoperator/poisson.hh>
-#include<dune/pdelab/common/vtkexport.hh>
+#include <dune/pdelab/localoperator/diffusion.hh>
+#include <dune/pdelab/common/vtkexport.hh>
+#include <dune/grid/io/file/vtk/subsamplingvtkwriter.hh>
 
 #include<typeinfo>
 
 #include "functionmacros.hh"
+#include "../../dune-pdelab-howto/examples/example05_operator.hh"
+
+template<typename GV, typename RF>
+class K
+  : public Dune::PDELab::AnalyticGridFunctionBase<Dune::PDELab::GridFunctionTraits<GV,RF,GV::dimension*GV::dimension,Dune::FieldMatrix<RF,GV::dimension,GV::dimension> >,
+                                                  K<GV,RF> >
+{
+public:
+  typedef Dune::PDELab::GridFunctionTraits<GV,RF,GV::dimension*GV::dimension,Dune::FieldMatrix<RF,GV::dimension,GV::dimension> > Traits;
+  typedef Dune::PDELab::AnalyticGridFunctionBase<Traits,K<GV,RF> > BaseT;
+
+  K (const GV& gv) : BaseT(gv) {}
+  inline void evaluateGlobal (const typename Traits::DomainType& x,
+							  typename Traits::RangeType& y) const
+  {
+    for (int i = 0; i < GV::dimension; ++i)
+      for (int j = 0; j < GV::dimension; ++j)
+        y[i][j] = i == j ? _permeability : 0.0;
+  }
+
+  K(const GV& gv, double permeability) :
+    BaseT(gv),
+    _permeability(permeability)
+  {}
+
+private:
+  const double _permeability;
+};
+
+// source term
+SIMPLE_ANALYTIC_FUNCTION(A0,x,y)
+{
+  y=0;
+}
+END_SIMPLE_ANALYTIC_FUNCTION
 
 // source term
 SIMPLE_ANALYTIC_FUNCTION(F,x,y)
 {
-  if (x[0]>0.25 && x[0]<0.375 && x[1]>0.25 && x[1]<0.375)
+  if (x[0]>0.75 && x[0]<0.875 && x[1]>0.25 && x[1]<0.375)
     y = 50.0;
   else
     y = 0.0;
-  y=0;
+  //y=0;
 }
 END_SIMPLE_ANALYTIC_FUNCTION
 
@@ -135,24 +172,27 @@ int main(int argc, char** argv) {
   typedef FEM::Traits::LocalFiniteElementType::Traits::
     LocalBasisType::Traits::RangeFieldType R;
 
-  FEM fem;
+  FEM fem0, fem1;
   typedef Dune::PDELab::NoConstraints NOCON;
-  typedef Dune::PDELab::ConformingDirichletConstraints CON;
   typedef Dune::PDELab::ISTLVectorBackend<1> VBE;
 
-  CON con;
-
   typedef Dune::PDELab::GridFunctionSpace<MDGV,FEM,NOCON,
-    Dune::PDELab::ISTLVectorBackend<1> > GFS;
+    Dune::PDELab::ISTLVectorBackend<1> > GFS0;
 
-  typedef GFS::ConstraintsContainer<R>::Type C;
+  typedef Dune::PDELab::GridFunctionSpace<SDGV,FEM,NOCON,
+    Dune::PDELab::ISTLVectorBackend<1> > GFS1;
+
+  typedef GFS0::ConstraintsContainer<R>::Type C;
   C cg;
 
-  GFS gfs(mdgv,fem);
+  GFS0 gfs0(mdgv,fem0);
+  GFS1 gfs1(sdgv1,fem1);
 
-  typedef Dune::PDELab::MultiDomain::MultiDomainGridFunctionSpace<Grid,GFS> MultiGFS;
+  typedef Dune::PDELab::MultiDomain::MultiDomainGridFunctionSpace<Grid,GFS0,GFS1> MultiGFS;
 
-  MultiGFS multigfs(grid,gfs);
+  MultiGFS multigfs(grid,gfs0,gfs1);
+
+  double d_0 = 0.00028, d_1 = 0.005, lambda = 1.0, sigma = 1.0, kappa = -0.05, tau = 0.1;
 
   typedef B<MDGV> BType;
   BType b(mdgv);
@@ -166,8 +206,17 @@ int main(int argc, char** argv) {
   typedef J<MDGV,R> JType;
   JType j(mdgv);
 
-  typedef Dune::PDELab::Poisson<FType,BType,JType,2> LOP;
-  LOP lop(f,b,j);
+  typedef K<MDGV,R> KType;
+  KType k(mdgv,d_0);
+
+  typedef A0<MDGV,R> A0Type;
+  A0Type a0(mdgv);
+
+  typedef Dune::PDELab::Diffusion<KType,A0Type,FType,BType,JType> LOP0;
+  LOP0 lop0(k,a0,f,b,j,2);
+
+  typedef Example05LocalOperator LOP1;
+  LOP1 lop1(d_0,d_1,lambda,sigma,kappa,2);
 
   typedef MDGV::IndexSet::SubDomainSet SDS;
   typedef Dune::PDELab::MultiDomain::EqualsSubDomains<SDS> EC;
@@ -175,32 +224,45 @@ int main(int argc, char** argv) {
   EC ec0(0);
   EC ec1(1);
 
-  typedef Dune::PDELab::MultiDomain::SubProblem<MultiGFS,CON,MultiGFS,CON,LOP,EC,0> SubProblem;
-  SubProblem sp0(con,con,lop,ec0);
-  SubProblem sp1(con,con,lop,ec1);
+  NOCON nocon;
 
+  typedef Dune::PDELab::MultiDomain::SubProblem<MultiGFS,NOCON,MultiGFS,NOCON,LOP0,EC,0> SubProblem0;
+  typedef Dune::PDELab::MultiDomain::SubProblem<MultiGFS,NOCON,MultiGFS,NOCON,LOP1,EC,0,1> SubProblem1;
+  SubProblem0 sp0(nocon,nocon,lop0,ec0);
+  SubProblem1 sp1(nocon,nocon,lop1,ec1);
+
+  /*
   SubProblem::Traits::LocalTrialFunctionSpace
     splfs0(multigfs,sp0,sp0.trialGridFunctionSpaceConstraints()),
     splfs1(multigfs,sp1,sp1.trialGridFunctionSpaceConstraints());
 
   constraints(b,multigfs,cg,b,splfs0,b,splfs1);
-
+  */
   // make coefficent Vector and initialize it from a function
   typedef MultiGFS::VectorContainer<R>::Type V;
   V x0(multigfs);
   x0 = 0.0;
+  /*
   Dune::PDELab::MultiDomain::interpolate(multigfs,x0,g,splfs0,g,splfs1);
   Dune::PDELab::set_shifted_dofs(cg,0.0,x0);
-
+  */
   typedef Dune::PDELab::ISTLBCRSMatrixBackend<1,1> MBE;
 
-  typedef Dune::PDELab::MultiDomain::MultiDomainGridOperatorSpace<MultiGFS,MultiGFS,MBE,SubProblem,SubProblem> MultiGOS;
+  typedef Dune::PDELab::MultiDomain::MultiDomainGridOperatorSpace<MultiGFS,MultiGFS,MBE,SubProblem0,SubProblem1> MultiGOS;
 
   MultiGOS multigos(multigfs,multigfs,cg,cg,sp0,sp1);
+
 
   typedef MultiGOS::MatrixContainer<R>::Type M;
   M m(multigos);
   m = 0.0;
+
+  for(int i = 0; i < m.base().N(); ++i) {
+    for(int j = 0; j < m.base().M(); ++j) {
+      std::cout << (m.base().exists(i,j) ? "X " : ". ");
+    }
+    std::cout << std::endl;
+  }
 
   multigos.jacobian(x0,m);
 
@@ -209,7 +271,7 @@ int main(int argc, char** argv) {
   r = 0.0;
 
   multigos.residual(x0,r);
-
+  /*
   Dune::MatrixAdapter<M,V,V> opa(m);
   Dune::SeqSSOR<M,V,V> ssor(m,1,1.0);
   Dune::CGSolver<V> solver(opa,ssor,1e-10,5000,2);
@@ -228,5 +290,5 @@ int main(int argc, char** argv) {
   Dune::VTKWriter<MDGV> vtkwriter(mdgv,Dune::VTKOptions::conforming);
   vtkwriter.addVertexData(new Dune::PDELab::VTKGridFunctionAdapter<DGF>(dgf,"solution"));
   vtkwriter.write("poisson.vtu",Dune::VTKOptions::ascii);
-
+  */
 }

a79378da8f593295f9b486f674949ed2c7b55a24
Steffen Muething <steffen.muething@ipvs.uni-stuttgart.de>
2010-07-07 17:58:10 +0200

    make operator_applier data modular and move class to separate header
    The operator_applier and the connected TMP machinery
    in multidomaingridfunctionspace.hh are also needed
    for the instationary MDGOS, but the data required
    by the functors invoked on the subproblems will differ
    in some cases. There will especially not be local
    function spaces all the time. In order to support that
    scenario and to increase flexibility for future use
    cases, this patch separates the invocation logic from
    the data carried by operator_applier. For that purpose,
    the data part of the class is split into many small
    purpose-built classes, from which the user can pick
    the required set and pass them as template parameters
    to operator_applier. Internally, the class then simply
    multiply inherits from all template parameters.
    
    As the resulting mechanism is fairly general, it
    can now be found in the new header operatorapplier.hh

---
+++ dune/pdelab/multidomain/operatorapplier.hh
@@ -0,0 +1,502 @@
+#ifndef DUNE_MULTIDOMAIN_OPERATORAPPLIER_HH
+#define DUNE_MULTIDOMAIN_OPERATORAPPLIER_HH
+
+#include <dune/pdelab/multidomain/typemap.hh>
+
+
+namespace Dune {
+
+namespace PDELab {
+
+namespace MultiDomain {
+
+template<typename MDGOS, typename Condition, template<bool,bool> class BooleanOp, bool start_value, std::size_t i, std::size_t n>
+struct child_condition
+{
+  static const bool value = BooleanOp<Condition::template test<typename MDGOS::template Child<i>::Type>::value,
+                                      child_condition<MDGOS,Condition,BooleanOp,start_value,i+1,n>::value
+                                     >::value;
+};
+
+template<typename MDGOS, typename Condition, template<bool,bool> class BooleanOp, bool start_value, std::size_t n>
+struct child_condition<MDGOS,Condition,BooleanOp,start_value,n,n>
+{
+  static const bool value = start_value;
+};
+
+template<bool a, bool b>
+struct and_
+{
+  static const bool value = a && b;
+};
+
+template<bool a, bool b>
+struct or_
+{
+  static const bool value = a || b;
+};
+
+template<typename MDGOS, typename Condition>
+struct all_childs : public child_condition<MDGOS,Condition,and_,true,0,MDGOS::CHILDREN> {};
+
+template<typename MDGOS, typename Condition>
+struct any_child : public child_condition<MDGOS,Condition,or_,false,0,MDGOS::CHILDREN> {};
+
+template<typename Applier, typename Operator, std::size_t i, std::size_t n>
+struct apply_operator_helper
+{
+  static void apply(const Applier& applier, Operator& op)
+  {
+    op(applier,applier.gos().template getChild<i>());
+    apply_operator_helper<Applier,Operator,i+1,n>::apply(applier,op);
+  }
+};
+
+// end of recursion
+template<typename Applier, typename Operator, std::size_t n>
+struct apply_operator_helper<Applier, Operator, n,n>
+{
+  static void apply(const Applier& applier, Operator& op)
+  {
+  }
+};
+
+
+template<typename Applier, typename Operator, std::size_t i, bool do_apply>
+struct conditional_apply;
+
+template<typename Applier, typename Operator, std::size_t i>
+struct conditional_apply<Applier,Operator,i,true>
+{
+  static void apply(const Applier& applier, Operator& op)
+  {
+    op(applier,applier.gos().template getChild<i>());
+  }
+};
+
+template<typename Applier, typename Operator, std::size_t i>
+struct conditional_apply<Applier,Operator,i,false>
+{
+  static void apply(const Applier& applier, Operator& op)
+  {
+  }
+};
+
+template<typename Applier, typename Condition, typename Operator, std::size_t i, std::size_t n>
+struct conditional_apply_operator_helper
+{
+  static void apply(const Applier& applier, Operator& op)
+  {
+    conditional_apply<Applier,Operator,i,Condition::template test<typename Applier::MDGOS::template Child<i>::Type>::value>::apply(applier,op);
+    conditional_apply_operator_helper<Applier,Condition,Operator,i+1,n>::apply(applier,op);
+  }
+};
+
+// end of recursion
+template<typename Applier, typename Condition, typename Operator, std::size_t n>
+struct conditional_apply_operator_helper<Applier, Condition, Operator, n,n>
+{
+  static void apply(const Applier& applier, Operator& op)
+  {
+  }
+};
+
+
+
+
+
+// data namespace ************************************************
+
+
+namespace data {
+
+template<typename MDGOS>
+class LocalTrialFunctionSpace
+{
+
+public:
+
+  typedef typename MDGOS::Traits::TrialGridFunctionSpace::LocalFunctionSpace LFSU;
+
+  LocalTrialFunctionSpace() :
+    _plfsu(NULL)
+  {}
+
+  void setlfsu(const LFSU& lfsu)
+  {
+    _plfsu = &lfsu;
+  }
+
+  const LFSU& lfsu() const
+  {
+    assert(_plfsu != NULL);
+    return *_plfsu;
+  }
+
+private:
+  const LFSU* _plfsu;
+};
+
+template<typename MDGOS>
+class LocalTestFunctionSpace
+{
+
+public:
+
+  typedef typename MDGOS::Traits::TestGridFunctionSpace::LocalFunctionSpace LFSV;
+
+  LocalTestFunctionSpace() :
+    _plfsv(NULL)
+  {}
+
+  void setlfsv(const LFSV& lfsv)
+  {
+    _plfsv = &lfsv;
+  }
+
+  const LFSV& lfsv() const
+  {
+    assert(_plfsv != NULL);
+    return *_plfsv;
+  }
+
+private:
+  const LFSV* _plfsv;
+};
+
+template<typename MDGOS>
+class LocalFunctionSpaces :
+    public LocalTrialFunctionSpace<MDGOS>,
+    public LocalTestFunctionSpace<MDGOS>
+{
+};
+
+
+template<typename MDGOS>
+class NeighborTrialFunctionSpace
+{
+
+public:
+
+  typedef typename MDGOS::Traits::TrialGridFunctionSpace::LocalFunctionSpace LFSU;
+
+  NeighborTrialFunctionSpace() :
+    _plfsun(NULL)
+  {}
+
+  void setlfsun(const LFSU& lfsun)
+  {
+    _plfsun = &lfsun;
+  }
+
+  const LFSU& lfsun() const
+  {
+    assert(_plfsun != NULL);
+    return *_plfsun;
+  }
+
+private:
+  const LFSU* _plfsun;
+};
+
+template<typename MDGOS>
+class NeighborTestFunctionSpace
+{
+
+public:
+
+  typedef typename MDGOS::Traits::TestGridFunctionSpace::LocalFunctionSpace LFSV;
+
+  NeighborTestFunctionSpace() :
+    _plfsvn(NULL)
+  {}
+
+  void setlfsvn(const LFSV& lfsvn)
+  {
+    _plfsvn = &lfsvn;
+  }
+
+  const LFSV& lfsvn() const
+  {
+    assert(_plfsvn != NULL);
+    return *_plfsvn;
+  }
+
+private:
+  const LFSV* _plfsvn;
+};
+
+template<typename MDGOS>
+class NeighborFunctionSpaces :
+    public NeighborTrialFunctionSpace<MDGOS>,
+    public NeighborTestFunctionSpace<MDGOS>
+{
+};
+
+template<typename MDGOS>
+class ElementReference
+{
+
+public:
+
+  typedef typename MDGOS::Traits::TrialGridFunctionSpace::Traits::GridType Grid;
+  typedef typename Grid::template Codim<0>::Entity Element;
+
+  ElementReference() :
+    _element(NULL)
+  {}
+
+  void setElement(const Element& element) {
+    _element = &element;
+  }
+
+  const Element& element() const {
+    assert(_element != NULL);
+    return *_element;
+  }
+
+private:
+  const Element* _element;
+
+};
+
+template<typename MDGOS>
+class IntersectionReference
+{
+
+public:
+
+  typedef typename MDGOS::Traits::TrialGridFunctionSpace::Traits::GridType Grid;
+  typedef typename Grid::LeafGridView::Intersection Intersection;
+
+  IntersectionReference() :
+    _intersection(NULL)
+  {}
+
+  void setIntersection(const Intersection& intersection, int intersectionIndex) {
+    _intersection = &intersection;
+    _intersectionIndex = intersectionIndex;
+  }
+
+  const Intersection& intersection() const {
+    assert(_intersection != NULL);
+    return *_intersection;
+  }
+
+  int intersectionIndex() const {
+    assert(_intersection != NULL);
+    return _intersectionIndex;
+  }
+
+
+private:
+  const Intersection* _intersection;
+  int _intersectionIndex;
+};
+
+template<typename MDGOS>
+class ElementSubDomains
+{
+
+public:
+
+  typedef typename MDGOS::Traits::TrialGridFunctionSpace::Traits::GridType Grid;
+  typedef typename Grid::Traits::LeafIndexSet::SubDomainSet ElementSubDomainSet;
+
+  const ElementSubDomainSet& elementSubDomains() const {
+    return _elementSet;
+  }
+
+  void setElementSubDomains(const ElementSubDomainSet& elementSet) {
+    _elementSet = elementSet;
+  }
+
+private:
+  ElementSubDomainSet _elementSet;
+
+};
+
+template<typename MDGOS>
+class NeighborSubDomains
+{
+
+public:
+
+  typedef typename MDGOS::Traits::TrialGridFunctionSpace::Traits::GridType Grid;
+  typedef typename Grid::Traits::LeafIndexSet::SubDomainSet ElementSubDomainSet;
+
+  const ElementSubDomainSet& neighborSubDomains() const {
+    return _neighborElementSet;
+  }
+
+  void setNeighborSubDomains(const ElementSubDomainSet& neighborElementSet) {
+    _neighborElementSet = neighborElementSet;
+  }
+
+private:
+  ElementSubDomainSet _neighborElementSet;
+
+};
+
+template<typename MDGOS>
+class SkeletonInvocationTracker
+{
+public:
+  SkeletonInvocationTracker() :
+    _alphaSkeletonInvoked(false)
+  {}
+
+  void setAlphaSkeletonInvoked() const {
+    _alphaSkeletonInvoked = true;
+  }
+
+  void clearAlphaSkeletonInvoked() {
+    _alphaSkeletonInvoked = false;
+  }
+
+  bool alphaSkeletonInvoked() const {
+    return _alphaSkeletonInvoked;
+  }
+
+private:
+  mutable bool _alphaSkeletonInvoked;
+
+};
+
+template<typename MDGOS>
+class ElementData :
+  public LocalFunctionSpaces<MDGOS>,
+  public ElementReference<MDGOS>,
+  public ElementSubDomains<MDGOS>
+{};
+
+template<typename MDGOS>
+class NeighborData :
+  public NeighborFunctionSpaces<MDGOS>,
+  public NeighborSubDomains<MDGOS>
+{};
+
+
+}
+
+template<typename MDGOS_, template<typename> class... Data>
+class operator_applier : public Data<MDGOS_>...
+{
+
+public:
+
+
+  typedef MDGOS_ MDGOS;
+
+  operator_applier(const MDGOS& mdgos) :
+    _mdgos(mdgos)
+  {}
+
+  template<typename Operator>
+  void operator()(Operator&& op)
+  {
+    apply_operator_helper<operator_applier,Operator,0,MDGOS::CHILDREN>::apply(*this,op);
+  }
+
+  template<typename Condition, typename Operator>
+  void conditional(Operator&& op)
+  {
+    conditional_apply_operator_helper<operator_applier,Condition,Operator,0,MDGOS::CHILDREN>::apply(*this,op);
+  }
+
+  const MDGOS& gos() const {
+    return _mdgos;
+  }
+
+private:
+
+  const MDGOS& _mdgos;
+
+};
+
+
+struct do_pattern_skeleton
+{
+  template<typename T>
+  struct test {
+    static const bool value = T::Traits::LocalOperator::doPatternSkeleton;
+  };
+};
+
+struct do_pattern_volume
+{
+  template<typename T>
+  struct test {
+    static const bool value = T::Traits::LocalOperator::doPatternVolume;
+  };
+};
+
+struct do_alpha_volume
+{
+  template<typename T>
+  struct test {
+    static const bool value = T::Traits::LocalOperator::doAlphaVolume;
+  };
+};
+
+struct do_alpha_skeleton
+{
+  template<typename T>
+  struct test {
+    static const bool value = T::Traits::LocalOperator::doAlphaSkeleton;
+  };
+};
+
+struct do_alpha_boundary
+{
+  template<typename T>
+  struct test {
+    static const bool value = T::Traits::LocalOperator::doAlphaBoundary;
+  };
+};
+
+struct do_alpha_skeleton_or_boundary
+{
+  template<typename T>
+  struct test {
+    static const bool value = T::Traits::LocalOperator::doAlphaSkeleton || T::Traits::LocalOperator::doAlphaBoundary;
+  };
+};
+
+struct do_lambda_volume
+{
+  template<typename T>
+  struct test {
+    static const bool value = T::Traits::LocalOperator::doLambdaVolume;
+  };
+};
+
+struct do_lambda_boundary
+{
+  template<typename T>
+  struct test {
+    static const bool value = T::Traits::LocalOperator::doLambdaBoundary;
+  };
+};
+
+struct do_alpha_volume_post_skeleton
+{
+  template<typename T>
+  struct test {
+    static const bool value = T::Traits::LocalOperator::doAlphaVolumePostSkeleton;
+  };
+};
+
+struct do_lambda_volume_post_skeleton
+{
+  template<typename T>
+  struct test {
+    static const bool value = T::Traits::LocalOperator::doLambdaVolumePostSkeleton;
+  };
+};
+
+} // namespace MultiDomain
+} // namespace PDELab
+} // namespace Dune
+
+#endif // DUNE_MULTIDOMAIN_OPERATORAPPLIER_HH

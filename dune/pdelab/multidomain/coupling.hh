b0b855c39ae084e5761d43e0c1ed505fea00ead9
Steffen Muething <steffen.muething@ipvs.uni-stuttgart.de>
2010-07-22 18:50:24 +0200

    add an initial version of a Coupling class

---
+++ dune/pdelab/multidomain/coupling.hh
@@ -0,0 +1,61 @@
+#ifndef DUNE_MULTIDOMAIN_COUPLING_HH
+#define DUNE_MULTIDOMAIN_COUPLING_HH
+
+namespace Dune {
+
+namespace PDELab {
+
+namespace MultiDomain {
+
+template<
+  typename LocalSubProblem_,
+  typename RemoteSubProblem_,
+  typename CouplingOperator_
+  >
+struct CouplingTraits
+{
+  typedef LocalSubProblem_ LocalSubProblem;
+  typedef RemoteSubProblem_ RemoteSubProblem;
+  typedef CouplingOperator_ CouplingOperator;
+};
+
+template<
+  typename LocalSubProblem,
+  typename RemoteSubProblem,
+  typename CouplingOperator
+  >
+class Coupling
+{
+
+public:
+
+  typedef CouplingTraits<LocalSubProblem,RemoteSubProblem,CouplingOperator> Traits;
+
+  const CouplingOperator& couplingOperator() const {
+    return _operator;
+  }
+
+  const LocalSubProblem& localSubProblem() const {
+    return _localSubProblem;
+  }
+
+  const RemoteSubProblem& remoteSubProblem() const {
+    return _remoteSubProblem;
+  }
+
+  template<typename SDS1, typename SDS2>
+  bool appliesTo(const SDS1& localSubDomains, const SDS2& remoteSubDomains) const {
+    return localSubProblem().appliesTo(localSubDomains) && remoteSubProblem().appliesTo(remoteSubDomains);
+  }
+
+private:
+  const LocalSubProblem& _localSubProblem;
+  const RemoteSubProblem& _remoteSubProblem;
+  const CouplingOperator& _operator;
+};
+
+} // namespace MultiDomain
+} // namespace PDELab
+} // namespace Dune
+
+#endif // DUNE_MULTIDOMAIN_COUPLING_HH

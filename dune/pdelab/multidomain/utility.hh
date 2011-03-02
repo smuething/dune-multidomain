73467977960021c8a9da7b48a9f9565a54313ffb
Steffen Muething <steffen.muething@ipvs.uni-stuttgart.de>
2011-03-02 21:06:18 +0100

    [Utility] Move indexed_transform TMP from variadiccompositenode.hh to utility.hh

--- dune/pdelab/multidomain/utility.hh
+++ dune/pdelab/multidomain/utility.hh
@@ -9,6 +9,73 @@ namespace PDELab {
 
 namespace MultiDomain {
 
+/**
+ * TMP for deriving storage types in VariadicCompositeNode
+ */
+template<typename... OArgs>
+struct replace;
+
+template<typename OHead, typename... OTail>
+struct replace<OHead,OTail...> {
+  template<typename Transform, typename... SArgs>
+  struct with {
+    typedef typename replace<OTail...>::template with<Transform,SArgs...,typename Transform::template transform<OHead>::type>::type type;
+  };
+};
+
+/**
+ * End of recursion - export the transformed argument list as a tuple
+ */
+template<>
+struct replace<> {
+  template<typename Transform, typename... SArgs>
+  struct with {
+    typedef typename Transform::template container<SArgs...>::type type;
+  };
+};
+
+/**
+ * wrapper to simplify calling the TMP
+ */
+template<typename Transform, typename... Args>
+struct transform {
+  typedef typename replace<Args...>::template with<Transform>::type type;
+};
+
+/**
+ * TMP for deriving storage types in VariadicCompositeNode
+ */
+template<std::size_t i, typename... OArgs>
+struct indexed_replace;
+
+template<std::size_t i, typename OHead, typename... OTail>
+struct indexed_replace<i,OHead,OTail...> {
+  template<typename Transform, typename... SArgs>
+  struct with {
+    typedef typename indexed_replace<i+1,OTail...>::template with<Transform,SArgs...,typename Transform::template transform<i,OHead>::type>::type type;
+  };
+};
+
+/**
+ * End of recursion - export the transformed argument list as a tuple
+ */
+template<std::size_t i>
+struct indexed_replace<i> {
+  template<typename Transform, typename... SArgs>
+  struct with {
+    typedef typename Transform::template container<SArgs...>::type type;
+  };
+};
+
+/**
+ * wrapper to simplify calling the TMP
+ */
+template<typename Transform, typename... Args>
+struct indexed_transform {
+  typedef typename indexed_replace<0,Args...>::template with<Transform>::type type;
+};
+
+
 template<int Value, int... Pack>
 struct pack_contains;
 

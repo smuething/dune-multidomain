76361da4f30fdfa4786828d061784dd42cc6047b
Steffen Muething <steffen.muething@ipvs.uni-stuttgart.de>
2011-03-08 22:53:42 +0100

    [Interpolation] Fix broken interpolate()
    - interface is unchanged
    - move creation of MultiDomainLFS before creation
      of SubProblemLFSs, as these now need the MultiDomainLFS
      at construction time
    - apply visitor from standard PDELab instead of
      invoking old TMP
    - remove needlessly complicated struct
      interpolation_pairs and do the recursion over
      the (function,LFS) pairs using plain functions

--- dune/pdelab/multidomain/interpolate.hh
+++ dune/pdelab/multidomain/interpolate.hh
@@ -8,94 +8,38 @@ namespace Dune {
 namespace PDELab {
 namespace MultiDomain {
 
-template<typename... SubProblems>
-struct interpolation_pairs;
+// end of recursion
+template<typename IB, typename XG, typename Element>
+void interpolate_subproblems(const IB& ib, XG& xg, const Element& element)
+{}
 
-template<typename Function, typename SubProblemLFS, typename... SubProblems>
-struct interpolation_pairs<Function,SubProblemLFS,SubProblems...>
+template<typename IB, typename XG, typename Element, typename Function, typename SubProblemLFS, typename... SubProblems>
+void interpolate_subproblems(const IB& ib,
+                             XG& xg,
+                             const Element& element,
+                             const Function& f,
+                             SubProblemLFS& subProblemLFS,
+                             SubProblems&&... subProblems)
 {
-  typedef interpolation_pairs<SubProblems...> next_pair;
-
-  dune_static_assert(((is_subproblem<typename SubProblemLFS::Traits::SubProblem>::value == true)), "subproblem local function space parameter invalid");
-
-  template<typename LFS>
-  static void setupLFS(const LFS& lfs,
-                       const Function& f,
-                       const SubProblemLFS& subProblemLFS,
-                       const SubProblems&... subProblems)
-  {
-    subProblemLFS.setup(lfs);
-    next_pair::setupLFS(lfs,subProblems...);
-  }
-
-  template<typename LFS, typename IB, typename XG, typename Element, typename SubDomainSet>
-  static void interpolate(const LFS& lfs,
-                          const IB& ib,
-                          XG& xg,
-                          const Element& element,
-                          const SubDomainSet& subDomainSet,
-                          const Function& f,
-                          const SubProblemLFS& subProblemLFS,
-                          const SubProblems&... subProblems)
-  {
-    if (subProblemLFS.appliesTo(subDomainSet))
+  if (subProblemLFS.appliesTo(element))
     {
       subProblemLFS.bind();
-      InterpolateVisitNodeMetaProgram<IB,Function,Function::isLeaf,SubProblemLFS,SubProblemLFS::isLeaf>
-        ::interpolate(ib,f,subProblemLFS,xg,element);
+      Dune::PDELab::TypeTree::applyToTreePair(f,subProblemLFS,InterpolateVisitor<IB,typename Element::Entity,XG>(ib,element.entity(),xg));
     }
-    next_pair::interpolate(lfs,ib,xg,element,subDomainSet,subProblems...);
-  }
-
-};
-
-template<typename UnpairedParameter>
-struct interpolation_pairs<UnpairedParameter>
-{
-  dune_static_assert(AlwaysFalse<UnpairedParameter>::value, "incomplete Function/SubProblemLFS pair in interpolate()");
-};
-
-template<>
-struct interpolation_pairs<>
-{
-
-  template<typename LFS>
-  static void setupLFS(const LFS& lfs)
-  {
-  }
-
-  template<typename LFS, typename IB, typename XG, typename Element, typename SubDomainSet>
-  static void interpolate(const LFS& lfs,
-                          const IB& ib,
-                          XG& xg,
-                          const Element& element,
-                          const SubDomainSet& subDomainSet)
-  {
-  }
-
-};
-
+  interpolate_subproblems(ib,xg,element,subProblems...);
+}
 
 // interpolation from a given grid function
-template<typename GFS, typename XG, typename... SubProblems>
-void interpolate(const GFS& gfs, XG& xg, const SubProblems&... subProblems)
+template<typename GFS, typename LFS, typename XG, typename... SubProblems>
+void interpolate(const GFS& gfs, LFS& lfs, XG& xg, SubProblems&&... subProblems)
 {
-  typedef interpolation_pairs<SubProblems...> _SubProblems;
-
-  // this is the leaf version now
 
   // get some types
   typedef typename GFS::Traits::GridType::LeafGridView GV;
   typedef typename GV::Traits::template Codim<0>::Iterator ElementIterator;
 
-  // make local function space
-  typedef typename GFS::LocalFunctionSpace LFS;
-  LFS lfs(gfs);
-
   // get index set
   const typename GV::IndexSet& is=gfs.gridview().indexSet();
-
-  _SubProblems::setupLFS(lfs,subProblems...);
 
   // loop once over the grid
   for (ElementIterator it = gfs.gridview().template begin<0>();
@@ -104,7 +48,9 @@ void interpolate(const GFS& gfs, XG& xg, const SubProblems&... subProblems)
       // bind local function space to element
       lfs.bind(*it);
       // call interpolate
-      _SubProblems::interpolate(lfs,InterpolateBackendStandard(),xg,*it,is.subDomains(*it),subProblems...);
+      interpolate_subproblems(InterpolateBackendStandard(),xg,
+                              ElementWrapper<GV>(*it,is.subDomains(*it)),
+                              subProblems...);
     }
 }
 
@@ -112,61 +58,17 @@ void interpolate(const GFS& gfs, XG& xg, const SubProblems&... subProblems)
 template<typename GFS, typename XG, typename... SubProblems>
 void interpolateOnTrialSpace(const GFS& gfs, XG& xg, const SubProblems&... subProblems)
 {
-  interpolate(gfs,xg,extract_trial_lfs(subProblems)...);
+  LocalFunctionSpace<GFS> lfs(gfs);
+  interpolate(gfs,lfs,xg,extract_trial_lfs(lfs,subProblems)...);
 }
 
 template<typename GFS, typename XG, typename... SubProblems>
 void interpolateOnTestSpace(const GFS& gfs, XG& xg, const SubProblems&... subProblems)
 {
-  interpolate(gfs,xg,extract_test_lfs(subProblems)...);
+  LocalFunctionSpace<GFS> lfs(gfs);
+  interpolate(gfs,lfs,xg,extract_test_lfs(lfs,subProblems)...);
 }
 
-
-// interpolation from a given grid function, using the global interface of the local finite element
-template<typename GFS, typename XG, typename... SubProblems>
-void interpolateGlobal(const GFS& gfs, XG& xg, const SubProblems&... subProblems)
-{
-  typedef interpolation_pairs<SubProblems...> _SubProblems;
-
-  // this is the leaf version now
-
-  // get some types
-  typedef typename GFS::Traits::GridType::LeafGridView GV;
-  typedef typename GV::Traits::template Codim<0>::Iterator ElementIterator;
-  typedef InterpolateBackendGlobal<typename GV::Traits::template Codim<0>::Geometry> IB;
-
-  // make local function space
-  typedef typename GFS::LocalFunctionSpace LFS;
-  LFS lfs(gfs);
-
-  // get index set
-  const typename GV::IndexSet& is=gfs.gridview().indexSet();
-
-  _SubProblems::setupLFS(lfs,subProblems...);
-
-  // loop once over the grid
-  for (ElementIterator it = gfs.gridview().template begin<0>();
-       it!=gfs.gridview().template end<0>(); ++it)
-    {
-      // bind local function space to element
-      lfs.bind(*it);
-      // call interpolate
-      _SubProblems::interpolate(lfs,IB(it->geometry()),xg,*it,is.subDomains(*it),subProblems...);
-    }
-}
-
-
-template<typename GFS, typename XG, typename... SubProblems>
-void interpolateGlobalOnTrialSpace(const GFS& gfs, XG& xg, const SubProblems&... subProblems)
-{
-  interpolateGlobal(gfs,xg,extract_trial_lfs(subProblems)...);
-}
-
-template<typename GFS, typename XG, typename... SubProblems>
-void interpolateGlobalOnTestSpace(const GFS& gfs, XG& xg, const SubProblems&... subProblems)
-{
-  interpolateGlobal(gfs,xg,extract_test_lfs(subProblems)...);
-}
 
 } // namespace MultiDomain
 } // namespace PDELab

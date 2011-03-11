f743282caa63eb60a6cab81a6f1d91d9ef0489f0
Steffen Muething <steffen.muething@ipvs.uni-stuttgart.de>
2011-03-11 18:34:56 +0100

    [ConstraintsAssembler] Working functors

--- dune/pdelab/multidomain/constraintsfunctors.hh
+++ dune/pdelab/multidomain/constraintsfunctors.hh
@@ -3,8 +3,6 @@
 
 #include <dune/pdelab/constraints/constraints.hh>
 #include <dune/pdelab/common/typetree.hh>
-#include <dune/pdelab/gridoperator/common/localassemblerenginebase.hh>
-#include <dune/pdelab/multidomain/operatorflagtests.hh>
 #include <dune/pdelab/multidomain/datawrappers.hh>
 
 namespace Dune {
@@ -58,7 +56,7 @@ namespace functors {
   };
 
   template<typename Data>
-  struct skeleton_or_processor_or_boundary_constraints
+  struct skeleton_or_boundary_constraints
     : public data_accessor<Data>
   {
 
@@ -69,17 +67,24 @@ namespace functors {
     {
       if (!descriptor.appliesTo(data().ig().insideElement()))
         return;
-      if (!descriptor.appliesTo(data().ig().outsideElement()))
+      if (descriptor.appliesTo(data().ig().outsideElement()) &&
+          data().ig().oneSidedDirection()) // Constraints are always applied one-sided
         {
-          // handle subproblem boundary
-          typedef BoundaryConstraints<typename Data::IG,typename Data::CG> Visitor;
-          Dune::PDELab::TypeTree::applyToTreePair(descriptor.parameters(),descriptor.lfs_s(),
+          typedef SkeletonConstraints<typename Data::IG,typename Data::CG> Visitor;
+          Dune::PDELab::TypeTree::applyToTreePair(descriptor.lfs_s(),
+                                                  descriptor.lfs_n(),
+                                                  Visitor(data().ig(),data().cg()));
+        }
+      else
+        {
+          typedef BoundaryConstraints<typename Data::IG, typename Data::CG> Visitor;
+          Dune::PDELab::TypeTree::applyToTreePair(descriptor.parameters(),
+                                                  descriptor.lfs_s(),
                                                   Visitor(data().ig(),data().cg()));
         }
     }
 
   };
-
 
   template<typename Data>
   struct boundary_constraints
@@ -91,100 +96,34 @@ namespace functors {
     template<typename Descriptor>
     void operator()(Descriptor& descriptor)
     {
-      if (!descriptor.appliesTo(data().ig().insideElement())
+      if (!descriptor.appliesTo(data().ig().insideElement()))
           return;
       typedef BoundaryConstraints<typename Data::IG,typename Data::CG> Visitor;
-      Dune::PDELab::TypeTree::applyToTreePair(descriptor.parameters(),descriptor.lfs_s(),
+      Dune::PDELab::TypeTree::applyToTreePair(descriptor.parameters(),
+                                              descriptor.lfs_s(),
                                               Visitor(data().ig(),data().cg()));
     }
 
   };
 
-
-
-template<typename CG, typename... ConstraintsSpecifications>
-class ConstraintsAssemblerEngine
-  : public Dune::PDELab::TypeTree::VariadicCompositeNode<ConstraintsSpecifications...>
-  , public Dune::PDELab::LocalAssemblerEngineBase
-{
-
-  typedef Dune::PDELab::TypeTree::VariadicCompositeNode<ConstraintsSpecifications...> NodeT;
-
-public:
-
-  bool requireIntersections() const
+  template<typename Data>
+  struct processor_constraints
+    : public data_accessor<Data>
   {
-    return requireVSkeleton() || requireVBoundary();
-  }
 
-  bool requireIntersectionsTwoSided() const
-  {
-    return requireVBoundary();
-  }
+    using data_accessor<Data>::data;
 
-  bool requireVVolume() const
-  {
-    return any_child<ConstraintsAssemblerEngine,do_constraints_volume>::value;
-  }
+    template<typename Descriptor>
+    void operator()(Descriptor& descriptor)
+    {
+      if (!descriptor.appliesTo(data().ig().insideElement()))
+        return;
+      typedef ProcessorConstraints<typename Data::IG, typename Data::CG> Visitor;
+      Dune::PDELab::TypeTree::applyToTree(descriptor.lfs_s(),
+                                          Visitor(data().ig(),data().cg()));
+    }
 
-  bool requireVSkeleton() const
-  {
-    return any_child<ConstraintsAssemblerEngine,do_constraints_skeleton>::value ||
-      any_child<ConstraintsAssemblerEngine,do_constraints_boundary>::value ||
-      any_child<ConstraintsAssemblerEngine,do_constraints_processor>::value;
-  }
-
-  bool requireVBoundary() const
-  {
-    return any_child<ConstraintsAssemblerEngine,do_constraints_boundary>::value;
-  }
-
-  template<typename EG, typename LFSV>
-  void onBindLFSV(const EG& eg, const LFSV& lfsv)
-  {
-    apply(subProblemConstraints,BindSubProblemLFS_S());
-  }
-
-  template<typename IG, typename LFSV_N>
-  void onBindLFSVOutside(const IG& ig, const LFSV_N& lfsv_n)
-  {
-    typedef visitor<functors::bind:
-    TypeTree::applyToTree(subProblemConstraints,bind_subproblem_lfs_n());
-  }
-
-  template<typename EG, typename LFSV>
-  void assembleVVolume(const EG& eg, const LFSV& lfsv)
-  {
-    typedef visitor<functors::volume_constraints> Visitor;
-    applyConstraints(Visitor::add_data(wrap_eg(eg),wrap_lfsv(lfsv),wrap_cg(cg)));
-  }
-
-  template<typename IG, typename LFSV_S, typename LFSV_N>
-  void assembleVSkeleton(const IG& ig,
-                         const LFSV_S& lfsv_s,
-                         const LFSV_N& lfsv_n)
-  {
-    typedef visitor<functors::skeleton_or_processor_or_boundary_constraints> Visitor;
-    applyConstraints(Visitor::add_data(wrap_ig(ig),wrap_lfsv_s(lfsv_s),wrap_lfsv_n(lfsv_n),wrap_cg(cg)));
-  }
-
-  template<typename IG, typename LFSV>
-  void assembleVBoundary(const IG& ig, const LFSV& lfsv)
-  {
-    typedef visitor<functors::boundary_constraints> Visitor;
-    applyConstraints(Visitor::add_data(wrap_eg(ig),wrap_lfsv(lfsv),wrap_cg(cg)));
-  }
-
-  ConstraintsAssemblerEngine(CG& cg_, ConstraintsSpecifications&... constraintsSpecifications)
-    : NodeT(constraintsSpecifications...)
-    , cg(cg_)
-  {}
-
-private:
-
-  CG& cg;
-
-};
+  };
 
 } // namespace functors
 

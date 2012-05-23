smuething/dune-multidomain

commit aab171f3c4418cd02d1af96738f4a16548b6f5c6

[navierstokesdarcy-analytic] Improve run-time configurability
- read output file names from parameter file
- read interface position from parameter file. Only -0.5, 0 and 0.5
   make sense with the default grid.

test/navierstokesdarcy-analytic.cc
@@ -705,10 +705,16 @@ int main(int argc, char** argv) {
     MDGV mdgv = grid.leafView();
     SDGV stokesGV = stokesGrid.leafView();
     SDGV darcyGV = darcyGrid.leafView();
+
+    typedef MDGV::Grid::ctype DF;
+    typedef double RF;
+
+    const DF interface_position = parameters.get<DF>("mesh.interface-position");
+
     grid.startSubDomainMarking();
     for (MDGV::Codim<0>::Iterator it = mdgv.begin<0>(); it != mdgv.end<0>(); ++it)
       {
-        grid.addToSubDomain(elementIndexToPhysicalGroup[mdgv.indexSet().index(*it)] > 0 ? 0 : 1,*it);
+        grid.addToSubDomain(elementIndexToPhysicalGroup[mdgv.indexSet().index(*it)] > interface_position ? 0 : 1,*it);
         // grid.addToSubDomain(0,*it);
       }
     grid.preUpdateSubDomains();
@@ -717,8 +723,6 @@ int main(int argc, char** argv) {
 
     grid.globalRefine(parameters.get<int>("mesh.refine"));
 
-    typedef MDGV::Grid::ctype DF;
-    typedef double RF;
 
     const int stokes_k = 2;
     const int stokes_q = 2*stokes_k;
@@ -922,7 +926,7 @@ int main(int argc, char** argv) {
         "",
         Dune::PDELab::MultiDomain::subdomain_predicate<Grid::SubDomainIndexType>(stokesGV.grid().domain())
       );
-      vtkwriter.write("stokes",Dune::VTKOptions::binaryappended);
+      vtkwriter.write(parameters["io.stokesfile"],Dune::VTKOptions::binaryappended);
     }
 
     {
@@ -934,7 +938,7 @@ int main(int argc, char** argv) {
         "",
         Dune::PDELab::MultiDomain::subdomain_predicate<Grid::SubDomainIndexType>(darcyGV.grid().domain())
       );
-      vtkwriter.write("darcy",Dune::VTKOptions::binaryappended);
+      vtkwriter.write(parameters["io.darcyfile"],Dune::VTKOptions::binaryappended);
     }
 
 #if 0

test/navierstokesdarcy-analytic.ini
@@ -10,7 +10,10 @@ interface.alpha = 1
 
 [mesh]
 filename = stokesdarcy-analytic-vertical.msh
-refine = 2
+refine = 1
 refineoutput = 2
+interface-position = 0
+
+[io]
 stokesfile=navierstokesdarcy-analytic-stokes
 darcyfile=navierstokesdarcy-analytic-darcy

commit 66c1ea64b4066dddb2e749ea6de7110bedeb9f9d

[navierstokesdarcy-analytic] Improve domain setup configuration
Commit aab171f3c4 introduced a bug in the domain setup: It claimed
to make it possible to geometrically set the interface position, but in
reality used the interface value to look up a physical group from Gmsh.

This patch fixes the bug and also cleans up domain setup in general:

- new option interface-by-subdomain: Pick whether you want to pick
  up the subdomains (physical groups) from the Gmsh file
- interface-by-subdomain == true
  In this case, you have to set stokes-subdomain-id and darcy-subdomain-id
  parameters to the appropiate values and the code will set up subdomains
  corresponding to those physical groups
- interface-by-subdomain == false
  In this case, the subdomains are assigned based on the x coordinate of the
  cell center. If center.x > interface-position, the cell becomes part of the
  Stokes domain, otherwise it will be assigned to the Darcy domain.

test/navierstokesdarcy-analytic.cc
@@ -710,13 +710,28 @@ int main(int argc, char** argv) {
     typedef MDGV::Grid::ctype DF;
     typedef double RF;
 
-    const DF interface_position = parameters.get<DF>("mesh.interface-position");
+    const bool interface_by_subdomain = parameters.get<bool>("mesh.interface-by-subdomain");
+    const DF interface_position = parameters.get("mesh.interface-position",0.0);
+    const int stokes_domain_id = parameters.get("mesh.stokes-domain-id",0);
+    const int darcy_domain_id = parameters.get("mesh.darcy-domain-id",1);
+    assert(stokes_domain_id != darcy_domain_id && "Stokes and Darcy domain must have distinct ids");
 
     grid.startSubDomainMarking();
     for (MDGV::Codim<0>::Iterator it = mdgv.begin<0>(); it != mdgv.end<0>(); ++it)
       {
-        grid.addToSubDomain(elementIndexToPhysicalGroup[mdgv.indexSet().index(*it)] > interface_position ? 0 : 1,*it);
-        // grid.addToSubDomain(0,*it);
+        if (interface_by_subdomain)
+          {
+            int domain = elementIndexToPhysicalGroup[mdgv.indexSet().index(*it)];
+            if (domain == stokes_domain_id)
+              grid.addToSubDomain(0,*it);
+            if (domain == darcy_domain_id)
+              grid.addToSubDomain(1,*it);
+          }
+        else
+          {
+            auto x = it->geometry().center()[0];
+            grid.addToSubDomain(x > interface_position ? 0 : 1,*it);
+          }
       }
     grid.preUpdateSubDomains();
     grid.updateSubDomains();

test/navierstokesdarcy-analytic.ini
@@ -13,6 +13,9 @@ filename = stokesdarcy-analytic-vertical.msh
 refine = 1
 refineoutput = 2
 interface-position = 0
+interface-by-subdomain = true
+stokes-domain-id = 1
+darcy-domain-id = 0
 
 [io]
 stokesfile=navierstokesdarcy-analytic-stokes
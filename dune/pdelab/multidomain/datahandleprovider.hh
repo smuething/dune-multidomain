c57ff4c7f8ee394965d01bf5ce355600504a8831
Steffen Müthing <muething@dune-project.org>
2014-06-29 15:41:44 +0200

    [DataHandleProvider] Adapt to upstream changes in PDELab
    PDELab commits d750a64449cb01f8cc0e668de135d771a772f096 and
    f6585524de4c88158dfb8baceab13c23a6ae947a changed the way the
    DataHandleProvider handles GFS size information. This patch contains the
    corresponding changes at the MultiDomain level.

--- dune/pdelab/multidomain/datahandleprovider.hh
+++ dune/pdelab/multidomain/datahandleprovider.hh
@@ -90,6 +90,81 @@ namespace Dune {
       };
 
 
+      template<typename Entity, typename Traits, typename OffsetIterator>
+      struct get_leaf_offsets_for_entity
+        : public TypeTree::DirectChildrenVisitor
+        , public TypeTree::StaticTraversal
+      {
+
+        typedef typename Traits::DOFIndex::EntityIndex EntityIndex;
+
+        template<typename GFS, typename Child, typename TreePath, typename ChildIndex>
+        void beforeChild(const GFS& gfs, const Child& child, TreePath tp, ChildIndex childIndex)
+        {
+          accumulate_size(child,
+                          gfs.ordering().template child<ChildIndex::value>(),
+                          typename gfs_flavor_tag<Child>::type());
+        }
+
+
+        template<typename GFS, typename Ordering>
+        void accumulate_size(const GFS& gfs, const Ordering& ordering, MultiDomainGFSTag tag)
+        {
+          Dune::PDELab::get_leaf_offsets_for_entity<EntityIndex,OffsetIterator> get_leaf_offsets(_entity_index,_oit);
+          TypeTree::applyToTree(ordering,get_leaf_offsets);
+
+          _oit = get_leaf_offsets.offsetIterator();
+        }
+
+        template<typename GFS, typename Ordering>
+        void accumulate_size(const GFS& gfs, const Ordering& ordering, CouplingGFSTag tag)
+        {
+          Dune::PDELab::get_leaf_offsets_for_entity<EntityIndex,OffsetIterator> get_leaf_offsets(_entity_index,_oit);
+          TypeTree::applyToTree(ordering,get_leaf_offsets);
+
+          _oit = get_leaf_offsets.offsetIterator();
+        }
+
+        template<typename GFS, typename Ordering>
+        void accumulate_size(const GFS& gfs, const Ordering& ordering, SubDomainGFSTag tag)
+        {
+          typedef typename GFS::Traits::GridViewType::template Codim<Entity::codimension>::EntityPointer SDEP;
+          const SDEP ep = gfs.gridView().grid().subDomainEntityPointer(_entity);
+          if (!gfs.gridView().indexSet().contains(*ep))
+            return;
+
+          EntityIndex ei;
+
+          Traits::DOFIndexAccessor::GeometryIndex::store(
+            ei,
+            ep->type(),
+            gfs.gridView().indexSet().index(*ep)
+          );
+
+          Dune::PDELab::get_leaf_offsets_for_entity<EntityIndex,OffsetIterator> get_leaf_offsets(ei,_oit);
+          TypeTree::applyToTree(ordering,get_leaf_offsets);
+
+          _oit = get_leaf_offsets.offsetIterator();
+        }
+
+        get_leaf_offsets_for_entity(const Entity& entity,
+                                    const EntityIndex& entity_index,
+                                    OffsetIterator oit)
+          : _oit(oit)
+          , _entity(entity)
+          , _entity_index(entity_index)
+        {}
+
+      private:
+
+        OffsetIterator _oit;
+        const Entity& _entity;
+        const EntityIndex& _entity_index;
+
+      };
+
+
+
       template<typename Entity, typename Traits, bool map_dof_indices>
       struct indices_for_entity
         : public TypeTree::DirectChildrenVisitor
@@ -237,6 +312,11 @@ namespace Dune {
       // generic data handle interface
       //------------------------------
 
+      DUNE_CONSTEXPR bool sendLeafSizes() const
+      {
+        return true;
+      }
+
       /*! how many objects of type DataType have to be sent for a given entity
 
         Note: Only the sender side needs to know this size.
@@ -262,11 +342,12 @@ namespace Dune {
       }
 
       //! return vector of global indices associated with the given entity
-      template<typename Entity, typename ContainerIndex, typename DOFIndex, bool map_dof_indices>
-      size_type dataHandleIndices (const Entity& e,
-                                   std::vector<ContainerIndex>& container_indices,
-                                   std::vector<DOFIndex>& dof_indices,
-                                   std::integral_constant<bool,map_dof_indices> map_dof_indices_value) const
+      template<typename Entity, typename ContainerIndex, typename DOFIndex, typename OffsetIterator, bool map_dof_indices>
+      void dataHandleIndices (const Entity& e,
+                              std::vector<ContainerIndex>& container_indices,
+                              std::vector<DOFIndex>& dof_indices,
+                              OffsetIterator oit,
+                              std::integral_constant<bool,map_dof_indices> map_dof_indices_value) const
       {
         typedef typename GFS::Ordering Ordering;
 
@@ -289,12 +370,17 @@ namespace Dune {
           gfs().gridView().indexSet().index(e)
         );
 
-        get_size_for_entity<Entity,typename Ordering::Traits> get_size(e,ei);
-        TypeTree::applyToTree(gfs(),get_size);
+        get_leaf_offsets_for_entity<Entity,typename Ordering::Traits,OffsetIterator> get_offsets(e,ei,oit);
+        TypeTree::applyToTree(gfs(),get_offsets);
+        OffsetIterator end_oit = oit + (TypeTree::TreeInfo<Ordering>::leafCount + 1);
 
-        container_indices.resize(get_size.size());
+        // convert sizes to offsets - last entry contains total size
+        std::partial_sum(oit,end_oit,oit);
+        size_type size = *(oit + TypeTree::TreeInfo<Ordering>::leafCount);
 
-        this->setup_dof_indices(dof_indices,get_size.size(),ei,map_dof_indices_value);
+        container_indices.resize(size);
+
+        this->setup_dof_indices(dof_indices,size,ei,map_dof_indices_value);
 
         indices_for_entity<
           Entity,
@@ -303,7 +389,6 @@ namespace Dune {
           > extract_indices(e,ei,container_indices.begin(),this->dof_indices_begin(dof_indices,map_dof_indices_value));
         TypeTree::applyToTree(gfs(),extract_indices);
 
-        return get_size.size();
       }
 
     };
